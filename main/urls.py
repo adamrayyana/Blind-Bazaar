f496235a7cd08a5b91f404d4c2ba29a085a4700b
Adam Rayyan <adam.rayyan.a@gmail.com>
2025-09-09 11:48:37 +0700

    add template, static CSS, and index routing

---
+++ main/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import show_index
+
+urlpatterns = [
+    path('', show_index, name='index'),
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_index(request):
+    context = {
+        'npm': '2406496031',
+        'name': 'Adam Rayyan Aryasatya',
+        'class': 'PBP A',
+    }
+    return render(request, 'main.html', context)
